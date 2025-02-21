ac494e6c0fac56652b2dc41c0244d55e51535e40
Luis Sandoval <ljulian199914@gmail.com>
2025-02-20 22:40:27 -0500

    Se creo el mapa de cantidad de gente en el mundo coon datos del 2005

--- map1.py
+++ map1.py
@@ -7,19 +7,33 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
-
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+    
 #Map()
 #Generar una base mapa de ancho(-80 a 80) y alto(-180 a 180) dados con cualquiera de los valores predeterminados mosaicos o una URL de mosaico personalizada.
-map = folium.Map(location=[38.58,-99.09],zoom_start=7, tiles="OpenStreetMap", attr="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.")
+map = folium.Map(location=[38.58,-99.09],zoom_start=7, tiles="CartoDB Positron", attr="Map data © OpenStreetMap contributors, Tiles © CartoDB")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el)+" msnm", parse_html=True) , icon=folium.Icon(color="green")))
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius=5, popup=folium.Popup(str(el)+" msnm", parse_html=True), color="grey", fill = True, fill_color=color_producer(el), fill_opacity=0.7 ))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding='utf-8-sig').read(), 
+                             style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005']< 20000000 else 'red'}))
+
 
 #popup=folium.Popup(str(el), parse_html=True)
-map.add_child(fg)
-
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("map1.html")
 
 
